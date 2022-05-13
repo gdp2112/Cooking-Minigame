6f6405f6ed4b30e20a13a4ea947cd8d9c039e602
gdp2112 <gdpennington2112@gmail.com>
2022-05-12 18:19:35 -0700

    Establish loop functions to enable and disable the ingredient buttons, call disable function when user selects 5th ingredient, rename 'clear' to 'reset', call enable function when user clicks 'reset',

--- cooking.js
+++ cooking.js
@@ -6,20 +6,47 @@ clear.disabled = true;
 const enter = document.querySelector('.enter');
 enter.disabled = true;
 
+function btnsDisable(){
+    for(i = 0; i < btns.length; i++){
+        btns[i].disabled = true;
+    }
+};
+
+function btnsEnable(){
+    for( i= 0; i < btns.length; i++){
+        btns[i].disabled = false;
+    }
+};
+
+function remove() {
+    for(i = ingredients.length; i > 0; i--){
+        ingredients.pop();
+    };
+};
+
 /--eventListeners for all ingredient buttons--/
 btns.forEach(btn => {
     btn.addEventListener('click', event => {
         ingredients.push(btn.textContent);
         para.textContent += btn.textContent + ', ';
+        if (ingredients.length > 0){
+            clear.disabled = false;
+            enter.disabled = false;
+        };
+        if (ingredients.length === 5){
+        btnsDisable();
+        }
     });
 })
 
 
+
 clear.addEventListener('click', event => {
-    for(i = ingredients.length; i > 0; i--){
-        ingredients.pop();
-    };
+    remove();
     para.textContent = 'Selected Ingredients: ';
+    clear.disabled = true;
+    enter.disabled = true;
+    btnsEnable();
 })
 
 
